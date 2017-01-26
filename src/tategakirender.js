0acf77dcf00705cb62b67f60a086db3c112537d1
pandanoir <moon_in_france@yahoo.co.jp>
2017-01-26 11:09:29 +0900

    fixed render algorithm

--- src/tategakirender.js
+++ src/tategakirender.js
@@ -1,5 +1,4 @@
 const eaw = require('eastasianwidth');
-import {log} from './log.js';
 export const clearScreen = () => {
     const rows = process.stdout.rows;
     let clearer = '';
@@ -27,40 +26,31 @@ export const writeLines = (lines, _relativeCursor) => {
     };
 
     lines = lines.slice(0, columns).map(line => [...line].slice(0, rows));
-    const display = [...Array(rows)].map(_ => Array(columns).fill(''));
+    const displayLines = [...Array(rows)].map(_ => Array(columns).fill(''));
     const lineWidth = [...Array(columns)].fill(0);
-    for (let i = 0, _i = Math.min(lines.length, columns); i < _i; i++) {
+    for (let i = 0, _i = Math.min(columns, lines.length); i < _i; i++) {
         lineWidth[i] = 1;
         for (let j = 0, _j = Math.min(rows, lines[i].length); j < _j; j++) {
-            display[j][columns - 1 - i] = lines[i][j];
             lineWidth[i] = Math.max(lineWidth[i], eaw.characterLength(lines[i][j]));
         }
     }
-    for (let i = 0; i < rows; i++) {
-        for (let j = columns - 1; j > 0; j--) {
-            const s = display[i][columns - 1 - j];
-            if (s !== '') display[i][columns - 1 - j] = ' '.repeat(lineWidth[j] - eaw.characterLength(s)) + s;
-        }
-    }
     let lastLine = 0, lineSum = 0;
-    for (let _i = lineWidth.length ; lastLine < _i; lastLine++) {
+    for (let _i = Math.min(columns, lines.length); lastLine < _i; lastLine++) {
         if (lineSum + lineWidth[lastLine] > columns) break;
         lineSum += lineWidth[lastLine];
     }
-    log(`lineWidth=${lineWidth}`);
-    log(`lineSum=${lineSum}`);
-    log(`columns=${columns}`);
     for (let i = 0; i < rows; i++) {
         process.stdout.write(' '.repeat(columns - lineSum));
-        for (let j = 0; j < lastLine; j++) {
-            if (relativeCursor.tate === i && relativeCursor.yoko === lastLine - 1 - j) {
-                setCursorStyle();
-                process.stdout.write(display[i][j] || ' '.repeat(lineWidth[lastLine - 1 - j]));
-                setStyle();
+        for (let j = lastLine - 1; j >= 0; j--) {
+            if (relativeCursor.tate === i && relativeCursor.yoko === j) setCursorStyle();
+            if (i >= lines[j].length) {
+                process.stdout.write(' '.repeat(lineWidth[j]));
             } else {
-                const s = display[i][j];
-                process.stdout.write(s !== '' ? s : ' '.repeat(lineWidth[lastLine - 1 - j]));
+                if (relativeCursor.tate === i && relativeCursor.yoko === j) setCursorStyle();
+                if (!lines[j][i]) process.stdout.write(' '.repeat(lineWidth[j]));
+                else process.stdout.write(' '.repeat(lineWidth[j] - eaw.characterLength(lines[j][i])) + lines[j][i]);
             }
+            setStyle();
         }
         process.stdout.write('\n');
     }
